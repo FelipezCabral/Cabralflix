30ebee8e779a10a78561652b3de68905b9f819ca
Felipe Cabral <afqf1213@gmail.com>
2020-08-07 21:31:56 -0300

    Initial commit

---
+++ src/Pages/cadastro/Categoria/index.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descrição: '',
+    cor: '',
+  };
+  const [categorias, setcategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    // chave: nome, descrição, bla, bli
+    setValues({
+      ...values,
+      [chave]: valor, // nome: 'valor'
+    });
+  }
+
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }
+
+  useEffect(() => {
+    console.log('alo alo bolsonaru');
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setcategorias([
+          ...resposta,
+        ]);
+      });
+
+    // setTimeout(() => {
+    //  setcategorias([
+    //    ...categorias,
+    //    {
+    //      id: 1,
+    //      nome: 'Front End',
+    //      descrição: 'Uma categoria boa',
+    //      cor: '#cbd1ff',
+    //    },
+    //    {
+    //      id: 2,
+    //      nome: 'Back End',
+    //      descrição: 'Outra categoria boa',
+    //      cor: '#cbd1ff',
+    //    },
+  //    ]);
+  //  }, 4 * 1000);//
+  }, []);
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setcategorias([
+          ...categorias,
+          values,
+        ]);
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descrição"
+          value={values.descrição}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      {categorias.length === 0 && (
+      <div>
+        {/* Cargando... */}
+        Loading...
+      </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para Home
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './Pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './Pages/cadastro/video';
+import CadastroCategoria from './Pages/cadastro/Categoria';
+
+
+
+const Pagina404 = () => (<div>Página 404</div>)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />      
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
